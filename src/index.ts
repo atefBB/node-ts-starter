d2bb205380439669684290bb776f86ac89efec22
Kawther BOUKHRISS <kawther.boukhriss@aureskonnect.com>
2022-03-09 17:50:15 +0100

    refactor: export `host/port` const from `index` file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Application, json, urlencoded } from "express";
-
 import "dotenv/config";
+import cors from "cors";
 
 import "./modulesAliasConfig";
 
@@ -10,15 +10,12 @@ const app: Application = express();
 
 app.use(json());
 
-app.use(urlencoded({ extended: false }));
-registerRouter(app);
-
 app.use(cors());
 app.use(urlencoded({ extended: false }));
 registerRouter(app);
 
-const HOST = process.env.HOST ?? "localhost";
-const PORT = process.env.PORT ?? 5100;
+export const HOST = process.env.HOST ? process.env.HOST : "localhost";
+export const PORT = process.env.PORT ?? 5100;
 
 app.listen(PORT, () =>
     console.log(`Server listening on http://${HOST}:${PORT}`)
