b55e740c22b25948b7841fff9470d8f11ef0fff1
Kawther BOUKHRISS <kawther.boukhriss@aureskonnect.com>
2022-03-09 17:54:36 +0100

    feat: add `swagger` description in `index router` in `routes` index file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,38 @@
 import { Router } from "express";
 
-import { indexController } from "@controllers/index";
+import { PORT, HOST } from "@root";
 
-const indexRouter = Router();
+import { router } from "@routes/example";
 
-indexRouter.get("/", indexController);
+export default (app: Application) => {
+    const options = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "Start Node API",
+                version: "1.0.0",
+                description:
+                    "This page is dedicated to the route list in the application",
+            },
+            servers: [
+                {
+                    url: `http://${HOST}:${PORT}/api/v1`,
+                },
+            ],
+        },
+        apis: [
+            `${__dirname}/*/*${path.extname(path.basename(__filename))}`,
+            `${__dirname}/*/*/*${path.extname(path.basename(__filename))}`,
+        ],
+    };
 
-export default indexRouter;
+    const specs = swaggerJsDoc(options);
+
+    app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+    app.get("/", (_, res) => {
+        res.json({ message: "API Running ! " });
+    });
+
+    app.use("/api/v1", [router]);
+};
