84a994d6cc00234270ab1ca818d4fdf344712218
Kawther BOUKHRISS <kawther.boukhriss@aureskonnect.com>
2022-03-09 17:48:32 +0100

    build: add `root` path in `modules alias config` file

--- src/modulesAliasConfig.ts
+++ src/modulesAliasConfig.ts
@@ -1,6 +1,7 @@
 import moduleAlias from "module-alias";
 
 moduleAlias.addAliases({
+    "@root": __dirname,
     "@routes": `${__dirname}/routes`,
     "@controllers": `${__dirname}/controllers`,
     "@helpers": `${__dirname}/helpers`,
